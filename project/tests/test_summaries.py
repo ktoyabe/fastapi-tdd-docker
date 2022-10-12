f777d061e4c095c0a2b012fc0c180f9c2ef898f5
ktoyabe <ktoyabe@gmail.com>
2022-10-12 16:48:14 +0900

    add post invalid_json test-code

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -12,3 +12,17 @@ def test_create_summary(test_app_with_db):
     print(response)
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["url"] == "https://foo.bar"
+
+
+def test_create_summaries_invalid_json(test_app):
+    response = test_app.post("/summaries/", data=json.dumps({}))
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing"
+            }
+        ]
+    }
