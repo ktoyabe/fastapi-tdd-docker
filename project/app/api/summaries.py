04b249845fdcbe6dfbaeebb7c3477e2b04034ee9
ktoyabe <ktoyabe@gmail.com>
2022-10-12 17:00:26 +0900

    fix response_model post /summaries

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -7,7 +7,7 @@ from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 router = APIRouter()
 
 
-@router.post("/", response_model=SummaryPayloadSchema, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=SummaryResponseSchema, status_code=status.HTTP_201_CREATED)
 async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
 
